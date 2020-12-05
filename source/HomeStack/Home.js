5c71e76682c464af4bae44983047dc04fb8c902c
David Grice <sonic5280@gmail.com>
2020-12-05 17:16:39 -0500

    Updated Home Screen

--- source/HomeStack/Home.js
+++ source/HomeStack/Home.js
@@ -1,9 +1,70 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, StyleSheet, View, FlatList, ImageBackground } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const Home = () => {
+    let [quote, setQuote] = useState([]);
+    let [firstName, setFirstName] = useState('');
+    let [lastName, setLastName] = useState('');
+
+    useEffect(() => {
+        fetch("https://quotes.rest/qod?language=en", {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                "X-TheySaidSo-Api-Secret": "myworkoutapptwo",
+            }
+        })
+        .then((response) => response.json())
+        .then((json) => setQuote(json.contents.quotes))
+        .catch((error) => {
+            console.log(error);
+        });
+
+        getAsyncData();
+    }, []);
+
+    const getAsyncData = async() => {
+        try {
+            AsyncStorage.getItem('userFirstName', function(errs, result) {
+                if (!errs) {
+                    if (result !== null) {
+                        console.log('userFirstName ' + result) // first name retrieved!
+                    }
+                }
+            }).then((firstName) => setFirstName(firstName));
+            AsyncStorage.getItem('userLastName', function(errs, result) {
+                if (!errs) {
+                    if (result !== null) {
+                        console.log('userLastName ' + result) // last name retrieved!
+                    }
+                }
+            }).then((lastName) => setLastName(lastName));
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <Text>Home.js</Text>
+        <View style={styles.mainView}>
+            <FlatList
+                data={quote}
+                keyExtractor={({ quote }) => quote}
+                renderItem={({ item }) => (
+                    <View key={item.date}>
+                        <Text style={styles.welcomeText}>Welcome</Text>
+                        <Text style={styles.welcomeUser}>{firstName} {lastName}</Text>
+                        <View style={styles.imageHolder}>
+                            <ImageBackground style={styles.mainImage} source={{uri:item.background}}>
+                                <Text key={item.date} style={styles.mainQuote}>{item.quote}</Text>
+                                <Text key={item} style={styles.mainAuthor}>{item.author}</Text>
+                            </ImageBackground>
+                        </View>
+                    </View>
+                )}
+            />
+        </View>
     )
 }
 
@@ -11,4 +72,62 @@ export default Home;
 
 const styles = StyleSheet.create({
 
+    mainView: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#44564a',
+        height: '100%',
+    },
+
+    welcomeText: {
+        marginTop: 0,
+        fontSize: 70,
+        fontWeight: 'bold',
+        fontFamily: 'Arial',
+        textAlign: 'center',
+        width: '100%',
+        color: '#e1c281',
+        zIndex: 1,
+    },
+
+    welcomeUser: {
+        marginTop: 0,
+        fontSize: 40,
+        fontWeight: 'bold',
+        fontFamily: 'Arial',
+        textAlign: 'center',
+        width: '100%',
+        color: '#e1c281',
+        zIndex: 1,
+    },
+
+    imageHolder: {
+        width: '100%',
+        height: '100%',
+    },
+
+    mainImage: {
+        marginTop: 30,
+        width: '100%',
+        resizeMode: 'stretch',
+        alignItems: 'center',
+    },
+
+    mainQuote: {
+        marginTop: 0,
+        fontSize: 30,
+        fontFamily: 'Arial',
+        textAlign: 'center',
+        color: 'white',
+    },
+
+    mainAuthor: {
+        marginTop: 10,
+        fontSize: 18,
+        fontFamily: 'Arial',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: 'white',
+        zIndex: 1,
+    },
 });
